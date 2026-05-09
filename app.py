69c061deef10e16b8e9f96d01c6fd75c7d7290eb
jbs361203-LDB <jbs36_1203@js.hs.kr>
2026-05-09 23:16:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import requests
 import os
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -8,6 +9,13 @@ API_KEY = os.getenv("WEATHER_API_KEY")
 
 @app.route("/")
 def home():
+
+    # 현재 시간보다 1시간 전 사용
+    now = datetime.now() - timedelta(hours=1)
+
+    base_date = now.strftime("%Y%m%d")
+    base_time = now.strftime("%H00")
+
     url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst"
 
     params = {
@@ -15,8 +23,8 @@ def home():
         "pageNo": "1",
         "numOfRows": "10",
         "dataType": "JSON",
-        "base_date": "20260509",
-        "base_time": "2200",
+        "base_date": base_date,
+        "base_time": base_time,
         "nx": "55",
         "ny": "127"
     }
@@ -31,9 +39,12 @@ def home():
         items = data['response']['body']['items']['item']
 
         for item in items:
+
+            # 기온
             if item['category'] == 'T1H':
                 temperature = item['obsrValue']
 
+            # 습도
             if item['category'] == 'REH':
                 humidity = item['obsrValue']
 
